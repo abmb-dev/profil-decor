2e4f2414e2e5050be00d7a6ddd81d08d1e9379af
BarbaMatei <mateibarba@yahoo.com>
2024-02-23 16:13:46 +0200

    db creation

---
+++ profil-decor-server/Models/Discount.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace profil_decor_server.Models
+{
+    public class Discount
+    {
+        [Key]
+        public int Id { get; set; }
+        public decimal Percentage { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int? ProductId {  get; set; }
+        public Product? Product { get; set; }
+    }
+}
